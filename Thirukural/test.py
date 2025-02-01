87c6c67e8e50a3fa364703854e63afe163df817e
megalai41519 <132387491+megalai41519@users.noreply.github.com>
2025-02-01 07:02:40 +0000

    s

--- Thirukural/test.py
+++ Thirukural/test.py
@@ -1,5 +1,10 @@
 import pandas as pd
 df = pd.read_csv("Thirukural1.csv",delimiter=",")
 df.head()
-df.columns = df.columns.str.strip()l
+df.columns = df.columns.str.strip()
 print(df.columns)
+print(df.ChapterName)
+print(df.SectionName)
+print(df.Verse)
+print(df.Translation)
+print(df.Explanation)
